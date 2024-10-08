e804d41b43a7d9c8edfac35b5cd5f32d62965500
AmarNuhija <142606791+AmarNuhija@users.noreply.github.com>
2024-10-08 15:51:06 +0200

    Add files via upload

---
+++ Spiel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeftCenterRightGame
+{
+    public class Spiel
+    {
+        private Spieler _aktuellerSpieler;
+        private List<Spieler> _spielerListe;
+        private Gui _gui;
+        private Becher _becher;
+    }
+
+    public Spiel()
+    {
+        
+    }
+}
